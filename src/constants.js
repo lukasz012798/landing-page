3ac7fd2f39074a8e935c6575aa0f9d19723bfa8f
Łukasz Adamczyk <lukasz012798@gmail.com>
2021-10-02 22:31:11 +0200

    Cards constant

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+import {
+  faCreditCard,
+  faHandsHelping,
+  faLaptop,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const cards = [
+  {
+    name: "Increase sales",
+    icon: faCreditCard,
+    shortDesc: "Keep the kitchen busy",
+    desc: "Join a well-oiled marketing machine and watch the orders come in through your door and online.",
+  },
+  {
+    name: "Reach more customers",
+    icon: faHandsHelping,
+    shortDesc: "Meet them and keep them",
+    desc: "Attract new local customers and keep them coming back for more.",
+  },
+  {
+    name: "Use our services",
+    icon: faLaptop,
+    shortDesc: "For businesses big and small",
+    desc: "Whatever your size we have tools, business support and savings to help grow your business.",
+  },
+];
